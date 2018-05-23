1318507810c09f67c92611ef516b710c803f34fb
sky <ismatch@qq.com>
2018-05-23 14:31:20 +0800

    初始化框架

---
+++ Sky.SixSixSix.Web/Sky.HelloCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sky.HelloCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
